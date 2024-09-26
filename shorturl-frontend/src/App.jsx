0543e5fdba43321de6def86717f6dfdf9c0f4111
Niraj Kumar <nk931685@gmail.com>
2024-09-26 22:19:02 +0530

    adding favicon

--- shorturl-frontend/src/App.jsx
+++ shorturl-frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import logo from './assets/icons8-favicon-48.png'; // Ensure the path to your PNG is correct
+
 import './App.css'
 
 function App() {
-  
+    return (
+        <div>
+          <img src={logo} alt="Logo" />
+        </div>
+      );
+    
 }
 
 export default App
--- shorturl-frontend/src/main.jsx
+++ shorturl-frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Make sure this import is correct
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Added Navigate for redirection
 import Signup from "./components/signup/signup.jsx"; // Ensure the correct path is used
 import Login from './components/login/login.jsx';
 import ShortenUrl from './components/shorten/shorten.jsx';
@@ -11,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
+      <Route path="/" element={<Navigate to="/auth/login" />} />
         <Route path="/auth/signup" element={<Signup />} />
         <Route path='/auth/login' element = {<Login />} />
         <Route path='/api/shorten' element= {<ShortenUrl />} />
